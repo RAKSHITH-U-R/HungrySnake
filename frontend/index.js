4bdd98831ad471870789dc12e7e794172c231453
RAKSHITH-U-R <20pw27@psgtech.ac.in>
2022-06-20 08:34:11 +0530

    okokok

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@ const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
 // const socket = io('https://sleepy-island-33889.herokuapp.com/');
-const socket = io('https://stark-dawn-30225.herokuapp.com/');
+const socket = io('https://arcane-wildwood-11946.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
