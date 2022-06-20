5027fdc62769a98dee0ed5aa9aed8d915e750b2d
RAKSHITH-U-R <20pw27@psgtech.ac.in>
2022-06-20 07:30:29 +0530

    original

--- frontend/index.js
+++ frontend/index.js
@@ -44,8 +44,8 @@ function init() {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
 
-  canvas.width = 800;
-  canvas.height = 800;
+  canvas.width = 600;
+  canvas.height = 600;
 
   ctx.fillStyle = BG_COLOUR;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
